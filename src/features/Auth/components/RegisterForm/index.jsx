6af0c916f97c59f04ce22f5bc05ccec6d0a6a470
TranVanNghia <TranNghia170735@gmail.com>
2022-05-08 11:08:54 +0700

    [FE]_009_Validation_Email

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -43,7 +43,10 @@ function RegisterForm(props) {
           .test('should has at least two words', 'Please enter at least two words', (value) => {
             console.log('Value', value);
             return value.split(' ').length >= 2;
-        })
+        }),
+        email: yup.string()
+            .required('Please enter your email.')
+            .email('Please enter a valid ')
     });
 
     const form = useForm({
