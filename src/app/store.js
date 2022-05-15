26ce2b0d9ee352675e4412f86bf7ace02b84f481
TranVanNghia <TranNghia170735@gmail.com>
2022-05-15 23:37:36 +0700

    [FE] 013 Setup userSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
+
 import counterReducer from '../features/Counter/counterSlice';
+import userReducer from '../features/Auth/userSlice';
 const {configureStore} = require('@reduxjs/toolkit');
 
 const rootReducer = {
     count : counterReducer,
+    user : userReducer,
 }
 
 
---
+++ src/features/Auth/userSlice.js
@@ -0,0 +1,14 @@
+// import { createSlice } from "@reduxjs/toolkit";
+const {createSlice} = require('@reduxjs/toolkit');
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        current:{},
+        settings:{},
+    },
+    reducers: {},
+});
+
+const {reducer } = userSlice;
+export default reducer; // default export
