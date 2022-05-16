4fd6cce921045a2d198668eca856a8340cbfa9c6
TranVanNghia <TranNghia170735@gmail.com>
2022-05-17 00:17:29 +0700

    [FE] 015 Su dung async action tren component?

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -3,7 +3,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import userAPi from 'api/userApi';
 
-const register = createAsyncThunk(
+export const register = createAsyncThunk(
     'user/register',
     async (payload) => {
       // Call Api to register.
