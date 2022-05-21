8bb759ed34509d9c7dacff3bead94512ed9f17c2
TranVanNghia <TranNghia170735@gmail.com>
2022-05-21 16:19:44 +0700

    [FE] 026 Khoi tao redux state tu localStorage.

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -2,6 +2,7 @@
 
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import userAPi from 'api/userApi';
+import StorageKeys from 'constants/storage-keys';
 
 export const register = createAsyncThunk(
     'user/register',
@@ -10,8 +11,8 @@ export const register = createAsyncThunk(
       const data = await userAPi.register(payload);
 
       // Save data to local storage
-      localStorage.setItem('access_token', data.jwt);
-      localStorage.setItem('user', JSON.stringify(data.user));
+      localStorage.setItem(StorageKeys.TOKEN, data.jwt);
+      localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
 
       //return user data.
       return data.user;
@@ -25,8 +26,8 @@ export const login = createAsyncThunk(
       const data = await userAPi.login(payload);
 
       // Save data to local storage
-      localStorage.setItem('access_token', data.jwt);
-      localStorage.setItem('user', JSON.stringify(data.user));
+      localStorage.setItem(StorageKeys.TOKEN, data.jwt);
+      localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
 
       //return user data.
       return data.user;
@@ -36,7 +37,7 @@ export const login = createAsyncThunk(
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        current:{},
+        current: JSON.parse(localStorage.getItem(StorageKeys.USER)) || {}, 
         settings:{},
     },
     reducers: {},
