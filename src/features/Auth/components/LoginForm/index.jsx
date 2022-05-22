b96d5c913531848bd7634655a824ad80f836b59f
TranVanNghia <TranNghia170735@gmail.com>
2022-05-22 10:24:29 +0700

    Update

--- src/features/Auth/components/LoginForm/index.jsx
+++ src/features/Auth/components/LoginForm/index.jsx
@@ -1,122 +1,99 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import InputField from '../../../../components/form-controls/InputField';
-import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
-import { Avatar, Button, LinearProgress, Typography } from '@material-ui/core';
-import { LockOutlined } from '@mui/icons-material';
-import { makeStyles } from '@material-ui/core/styles';
-import PasswordField from '../../../../components/form-controls/PasswordField';
+import { Avatar, Button, LinearProgress, makeStyles, Typography } from '@material-ui/core';
+import { LockOutlined } from '@material-ui/icons';
+import InputField from 'components/form-controls/InputField';
+import PasswordField from 'components/form-controls/PasswordField';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'relative',
+    paddingTop: theme.spacing(4),
+  },
 
-const useStyles = makeStyles((theme) =>({
-    root: {
-        position:'relative',
-        paddingTop:theme.spacing(2),
-    },
+  avatar: {
+    margin: '0 auto',
+    backgroundColor: theme.palette.secondary.main,
+  },
 
-    avatar: {
-        margin: '0 auto',
-        backgroundColor: theme.palette.secondary.main
-    },
+  title: {
+    margin: theme.spacing(2, 0, 3, 0),
+    textAlign: 'center',
+  },
 
-    title: {
-        margin: theme.spacing(2, 0, 3, 0),
-        textAlign: 'center',
-    },
+  submit: {
+    margin: theme.spacing(3, 0, 2, 0),
+  },
 
-    submit: {
-        margin: theme.spacing(3, 0, 2, 0),
-    },
-
-    progress: {
-        position: 'absolute',
-        top: theme.spacing(1),
-        left: 0,
-        right:0,
-    }
-
+  progress: {
+    position: 'absolute',
+    top: theme.spacing(1),
+    left: 0,
+    right: 0,
+  },
 }));
 
 LoginForm.propTypes = {
-    onSubmit: PropTypes.func,
-
+  onSubmit: PropTypes.func,
 };
 
 function LoginForm(props) {
-    const classes = useStyles();
-    const schema = yup.object().shape({
-        fullName: yup.string()
-            .required('Please enter your full name')
-            .test('should has at least two words', 'Please enter at least two words', (value) => {
-             console.log('Value', value);
-             return value.split(' ').length >= 2;
-        }),
-        email: yup.string()
-            .required('Please enter your email.')
-            .email('Please enter a valid email address.'),
-        password: yup.string()
-            .required('Please enter your password ')
-            .min(6, 'Please enter at least 6 character.'),
-        retypePassword: yup.string()
-            .required('Please retype your password.')
-            .oneOf([yup.ref('password')],'Password does not match')       
-    });
+  const classes = useStyles();
 
-    const form = useForm({
-        defaultValues: {
-            fullName: '',
-            email:'',
-            password:'',
-            retypePassword:''
-        },
-        resolver: yupResolver(schema),
-    });
+  const schema = yup.object().shape({
+    identifier: yup.string().required('Please enter your email.').email('Please enter a valid email address.'),
+    password: yup.string().required('Please enter your password'),
+  });
+  const form = useForm({
+    defaultValues: {
+      identifier: '',
+      password: '',
+    },
+    resolver: yupResolver(schema),
+  });
 
-    const handleSubmit = async (values) => {
-        const {onSubmit} = props;
-        if (onSubmit) {
-          await onSubmit(values);
-        }
+  const handleSubmit = async (values) => {
+    const { onSubmit } = props;
+    if (onSubmit) {
+      await onSubmit(values);
     }
+  };
 
-    const {isSubmitting} = form.formState;
+  const { isSubmitting } = form.formState;
 
-    return (
-        
-        <div className={classes.root}>
-            {isSubmitting && <LinearProgress className={classes.progress}/>} 
+  return (
+    <div className={classes.root}>
+      {isSubmitting && <LinearProgress className={classes.progress} />}
 
+      <Avatar className={classes.avatar}>
+        <LockOutlined></LockOutlined>
+      </Avatar>
 
-            <Avatar className={classes.avatar}>
-                <LockOutlined></LockOutlined>
-            </Avatar>
+      <Typography className={classes.title} component="h3" variant="h5">
+        Sign In
+      </Typography>
 
-            <Typography className={classes.title} component="h3" variant="h5">
-                Create An Account
-            </Typography>
+      <form onSubmit={form.handleSubmit(handleSubmit)}>
+        <InputField name="identifier" label="Email" form={form} />
+        <PasswordField name="password" label="Password" form={form} />
 
-            <form onSubmit={form.handleSubmit(handleSubmit)}>
-                <InputField name="fullName" label="FullName" form={form} />
-                <InputField name="email" label="Email" form={form} />
-                <PasswordField name="password" label="Password" form={form} />
-                <PasswordField name="retypePassword" label="Retype Password" form={form} />
-                
-                <Button 
-                    disabled={isSubmitting} 
-                    type="submit" 
-                    className={classes.submit} 
-                    variant="contained" 
-                    color="primary" fullWidth
-                    size="large"
-                    >
-                    Create an account
-                </Button>
-            </form>
-        </div>
-   
-    );
+        <Button
+          disabled={isSubmitting}
+          type="submit"
+          className={classes.submit}
+          variant="contained"
+          color="primary"
+          fullWidth
+          size="large"
+        >
+          Sign in
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
