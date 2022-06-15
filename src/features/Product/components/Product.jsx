90fcc4c7c6cb3e3b0d217e6ab55d506683437e1b
TranVanNghia <TranNghia170735@gmail.com>
2022-06-16 00:12:58 +0700

    [FE][06] Xu ly thumbnail cho san pham

--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
+import { THUMBNAIL_PLACEHOLDER } from 'constants';
+import { STATIC_HOST } from 'constants';
 
 Product.propTypes = {
     product: PropTypes.object
 };
 
 function Product({product}) {
+    const thumbnailUrl = product.thumbnail
+    ? `${STATIC_HOST}${product.thumbnail?.url}`
+    : THUMBNAIL_PLACEHOLDER;
+
     return (
         <Box padding={1}>
-            <Skeleton variant ="react" width="100%" height={118}/>
+            <Box padding={1}>
+                <img src={thumbnailUrl} alt ={product.name} width="100%"/>
+            </Box>
             <Typography variant="body2">{product.name}</Typography>
             <Typography variant="body2">
                 {product.salePrice} -{product.promotionPercent}
