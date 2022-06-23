671a514fb98d8e1849a770daab986d4e89203147
TranVanNghia <TranNghia170735@gmail.com>
2022-06-23 22:48:45 +0700

    [FE][016] Loc theo khoang gia.

--- src/features/Product/components/ProductFilters.jsx
+++ src/features/Product/components/ProductFilters.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FilterByCategory from './Filters/FilterByCategory';
+import FilterByPrice from './Filters/FilterByPrice';
 
 ProductFilters.propTypes = {
     filters: PropTypes.object.isRequired,
@@ -13,15 +14,19 @@ function ProductFilters({filters, onChange}) {
         if(!onChange) return;
 
         const newFilters = {
-            ...filters,
             "category.id": newCategoryId
         }
         onChange(newFilters);
     };
 
+    const handlePriceChange = (values) => {
+        if(onChange) onChange(values)
+    }
+
     return (
         <div>
            <FilterByCategory onChange={handleCategoryChange}/> 
+           <FilterByPrice onChange={handlePriceChange}/>
         </div>
     );
 }
