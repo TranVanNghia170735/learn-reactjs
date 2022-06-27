9020a56220bd452f97c4f8b577c2683f3eed37cf
TranVanNghia <TranNghia170735@gmail.com>
2022-06-27 23:26:07 +0700

    [FE][022] Dong bo filters len URL

--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -8,6 +8,8 @@ import ProductList from '../components/ProductList';
 import ProductSort from '../components/ProductSort';
 import ProductFilters from '../components/ProductFilters';
 import FilterViewer from '../components/FilterViewer';
+import { useHistory, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 
 const useStyles = makeStyles(theme => ({
     root: {},
@@ -32,6 +34,11 @@ const useStyles = makeStyles(theme => ({
 
 function ListPage(props) {
     const classes = useStyles();
+
+    const history = useHistory();
+    const location = useLocation();
+    const queryParams = queryString.parse(location.search);
+
     const [productList, setProductList] = useState([]);
     const [pagination, setPagination] = useState({
         limit: 10,
@@ -39,11 +46,20 @@ function ListPage(props) {
         page: 1
     })
     const [loading, setLoading] = useState(true);
-    const [filters, setFilters] = useState({
-        _page: 1, 
-        _limit: 9,
-        _sort: 'salePrice:ASC'
-    });
+    const [filters, setFilters] = useState(() => ({
+        ... queryParams,
+        _page: Number.parseInt(queryParams._page) || 1, 
+        _limit: Number.parseInt(queryParams._limit) || 9,
+        _sort: queryParams._sort ||'salePrice:ASC'
+    }));
+
+    useEffect(() => {
+        history.push({
+            pathname: history.location.pathname,
+            search: queryString.stringify(filters) // Stringify an object into a query string and sort the keys.
+
+        })
+    }, [history, filters])
     
     useEffect(() => {
         (async () => {
