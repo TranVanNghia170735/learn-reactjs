d1a22d44048b946f6ac6555463edcd25cd93b584
TranVanNghia <TranNghia170735@gmail.com>
2022-07-04 00:13:57 +0700

    [004 + 005]  Tạo selectors cho cart items/ Handle nút add to cart

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 
 import counterReducer from '../features/Counter/counterSlice';
 import userReducer from '../features/Auth/userSlice';
+import cartReducer from '../features/Cart/cartSlice';
 const {configureStore} = require('@reduxjs/toolkit');
 
 const rootReducer = {
     count : counterReducer,
     user : userReducer,
+    cart: cartReducer,
 }
 
 
