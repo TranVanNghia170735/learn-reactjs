697bea08290dff6e29f24bf562d4ac7eb51b9d3b
TranVanNghia <TranNghia170735@gmail.com>
2022-07-04 22:36:57 +0700

    [FE][006] Them icon gio hang tren header.

--- src/features/Cart/index.jsx
+++ src/features/Cart/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { cartTotalSelector } from './selectors';
 
 CartFeatures.propTypes = {
     
 };
 
 function CartFeatures(props) {
+    const cartTotal = useSelector(cartTotalSelector);
+
     return (
         <div>
-            Cart Feature
+            Cart Feature {cartTotal}
         </div>
     );
 }
